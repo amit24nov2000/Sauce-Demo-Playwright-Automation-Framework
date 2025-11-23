a729ea6ed70f4b6c835e69b6a58bbfc8e293284d
amit24nov2000 <amit24nov2000@gmail.com>
2025-11-23 10:50:50 +0530

    adding env dotenv file

--- tests/auth/auth.setup.ts
+++ tests/auth/auth.setup.ts
@@ -2,8 +2,8 @@ import {test as setup, expect} from '@playwright/test';
 
 setup('Loging and Save storage state',async({page})=>{
     await page.goto('https://www.saucedemo.com/');
-    await page.getByPlaceholder('Username').fill('standard_user');
-    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByPlaceholder('Username').fill(process.env.USERNAME || '');
+    await page.getByPlaceholder('Password').fill(process.env.PASSWORD || '');
     await page.locator(`//*[@type="submit"]`).click();
     await page.waitForURL(`https://www.saucedemo.com/inventory.html`);
     await page.context().storageState({path:'storage/auth.json'});
